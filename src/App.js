1569e9c258c562db0ce6f1e1baf8be9512b1a27c
lishu gupta <lishugupta652@gmail.com>
2019-09-05 02:56:24 +0530

    header and portfolio added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
+//Components Imports
+import Header from './Components/Header'
+
+// CSS Files
+import './Css/App.css'
 function App() {
   return (
     <div className="App">
-      <h2>This is Pakka baniya on duty :) ----------</h2>
+
+      <Header />
+
+      <div className="portfolio" id="portfolio">
+        <div className="title"><h1>portfolio</h1></div>
+        <div className="line">
+          <div></div>
+        </div>
+        <div className="cards">
+
+          <div className="card">
+            <div className="inline">
+              <h1>Title</h1>
+              <h3>Img</h3>
+            </div>
+            <p className="desc">This is the nice desc of the technology.</p>    
+          </div>
+          
+          <div className="card">
+            <div className="inline">
+                <h1>Title</h1>
+                <h3>Img</h3>
+            </div>
+            <p className="desc">This is the nice desc of the technology.</p>
+          </div>
+
+          <div className="card">
+            <div className="inline">
+                <h1>Title</h1>
+                <h3>Img</h3>
+            </div>
+            <p className="desc">This is the nice desc of the technology.</p>
+          </div>
+
+          <div className="card">
+            <div className="inline">
+                <h1>Title</h1>
+                <h3>Img</h3>
+            </div>
+            <p className="desc">This is the nice desc of the technology.</p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            
+            <div className="nav">
+                <h1 className="title">@Pakka__Baniya</h1>
+                 <a className="down-icon" href="#portfolio">
+                     <i className="fa fa-arrow-circle-down " aria-hidden="true"></i>
+                 </a>
+            </div>
+
+        </>
+    )
+}
+
+export default Header
