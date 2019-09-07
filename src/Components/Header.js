65725297a7fe22aeda039c7017dd676b4b20bb7b
lishu gupta <lishugupta652@gmail.com>
2019-09-07 20:50:22 +0530

    new header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,39 @@
 import React from 'react'
-
+import { Link, animateScroll as scroll } from "react-scroll";
+import Image_icon from "../image/hero-img.jpg"
 const Header = () => {
     return (
         <>
-            
-            <div className="nav">
-                <h1 className="title">@Pakka__Baniya</h1>
-                 <a className="down-icon" href="#portfolio">
-                     <i className="fa fa-arrow-circle-down " aria-hidden="true"></i>
-                 </a>
-            </div>
+        <header>
+            <div className="app">
 
+            </div>
+                
+                <div className="banner">
+
+                <div style={{height: "90vh", overflow: "", position: "relative"}} >
+                    <svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: "100%", width: "100%"}}>
+                        <path d="M0.00,130.27 C216.00,182.92 300.30,60.39 500.00,139.03 L500.00,0.00 L0.00,0.00 Z" style={{stroke: "none", fill: "#333652"}}></path>
+                    </svg>
+
+                <div className="hero">
+                    <div className="hero-content">
+                        <img src={Image_icon} alt="lishu Gupta"/>
+                        <h2 className="name">Dimitri Marco</h2>
+                        <p className="info">Lorem lorem lorem ipsu is the greatets ihis kand thisk akjldf he lkajd iognakdjf oiejkaf dljka.</p>
+                        <i class="fa fa-angle-down" aria-hidden="true"></i>
+                        <i class="fa fa-arrow-circle-down" aria-hidden="true"></i>
+                        <i class="fa fa-arrow-circle-down" aria-hidden="true"></i>
+                        <i class="fa fa-arrow-circle-down" aria-hidden="true"></i>
+                        <i class="fa fa-arrow-circle-down" aria-hidden="true"></i>
+                        <i class="fa fa-arrow-circle-o-down" aria-hidden="true"></i>
+                    </div>
+                </div>
+                </div>
+                
+                
+            </div>
+        </header>
         </>
     )
 }
