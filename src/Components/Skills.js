07073daad0d0f0796fcea8f4bb29ec3feb1f232b
lishu gupta <lishugupta652@gmail.com>
2019-09-09 23:31:24 +0530

    animaition added in skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Bounce from 'react-reveal/Bounce';
+import Zoom from 'react-reveal/Zoom';
 
 const Skills = () => {
     return (
@@ -10,13 +12,17 @@ const Skills = () => {
 
             <div className="container">
                 <div className="top">
+                    <Bounce left>
                     <h1>SKILLS</h1>
+                    </Bounce>
                 </div>
 
                 <div className="grid">
                     <div className="one">
                         <div className="content">
-                            <h1>PYTHON</h1>
+                            <Zoom top>
+                                <h1>PYTHON</h1>
+                            </Zoom>
                         <div className="rotate">
                         <i class="devicon-python-plain colored"></i>
                         </div>
@@ -24,16 +30,22 @@ const Skills = () => {
                 </div>
 
                 <div className="two">
-                    Web Development
+                    <Zoom top>
+                        Web Development
+                    </Zoom>
                     <div className="padding-top-5">
                         <i class="devicon-html5-plain colored"></i>
                         <i class="devicon-css3-plain colored"></i>
                         <i class="devicon-javascript-plain colored"></i>
                         <i class="devicon-react-original colored"></i>
+                        <i class="devicon-bootstrap-plain colored"></i>
+
                     </div>
                     </div>
                     <div className="three">
-                        Android Development
+                        <Zoom top>
+                            Android Development
+                        </Zoom>
                         <div className="android">
                         <i class="devicon-android-plain-wordmark colored"></i>
                         </div>
