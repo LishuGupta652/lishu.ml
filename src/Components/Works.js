04c5e7311672aed8c4a4ab7838db1ce549e7dbb3
lishu gupta <lishugupta652@gmail.com>
2019-09-10 01:32:25 +0530

    works added

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import azaelindia from '../image/azaelindia.jpg'
+import dwarf from '../image/dwarf.jpg'
+import azaelindia from '../image/azaelindia.jpg';
+import payingguest from '../image/payingguest.jpg';
 
 const Works = () => {
     return (
-        <>
+        <div className="work-wrapper">
         <div className="work" id="work">
             <div className="container">
                 <h1>Works</h1>
@@ -13,15 +15,26 @@ const Works = () => {
                     <img src={azaelindia} alt="Azael HomePage" />
                 </a>
             </div>
-
         </div>
 
         <div className="guest">
-            <div className="container">
-                
+            <div className="content"> 
+            <a href="https://www.dwarf.ga">
+                <img src={dwarf}  alt="Dwarf Company"/>
+            </a>
+
             </div>
         </div>
-        </>
+
+        <div className="paying">
+            <div className="content"> 
+            <a href="https://www.payingguest.ml">
+                <img src={payingguest}  alt="Paying Gueset"/>
+            </a>
+
+            </div>
+        </div>
+        </div>
     )
 }
 
