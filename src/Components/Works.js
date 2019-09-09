399a51ae3c1d90b73f68b984272d90aea5c00b3d
lishu gupta <lishugupta652@gmail.com>
2019-09-10 02:05:08 +0530

    works completed

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -3,15 +3,19 @@ import dwarf from '../image/dwarf.jpg'
 import azaelindia from '../image/azaelindia.jpg';
 import payingguest from '../image/payingguest.jpg';
 
+import Bounce from 'react-reveal/Bounce';
+
 const Works = () => {
     return (
         <div className="work-wrapper">
         <div className="work" id="work">
             <div className="container">
-                <h1>Works</h1>
+                <Bounce left>
+                    <h1>Works</h1>
+                </Bounce>
             </div>
             <div className="right-img">
-                <a href="https://www.azaelindia.com" >
+                <a href="https://www.azaelindia.com" target="_blank" >
                     <img src={azaelindia} alt="Azael HomePage" />
                 </a>
             </div>
@@ -19,7 +23,7 @@ const Works = () => {
 
         <div className="guest">
             <div className="content"> 
-            <a href="https://www.dwarf.ga">
+            <a href="https://www.dwarf.ga" target="_blank">
                 <img src={dwarf}  alt="Dwarf Company"/>
             </a>
 
@@ -28,7 +32,7 @@ const Works = () => {
 
         <div className="paying">
             <div className="content"> 
-            <a href="https://www.payingguest.ml">
+            <a href="http://www.payingguest.ml" target="_blank">
                 <img src={payingguest}  alt="Paying Gueset"/>
             </a>
 
