88cfaec42cae9f23dfd7d88ef4e29836acd52bb0
lishu gupta <lishugupta652@gmail.com>
2019-09-10 15:22:04 +0530

    waste code

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -6,38 +6,22 @@ import payingguest from '../image/payingguest.jpg';
 import Bounce from 'react-reveal/Bounce';
 
 const Works = () => {
-    return (
-        <div className="work-wrapper">
-        <div className="work" id="work">
-            <div className="container">
+    return (   
+    <div className="work">
+        
+            <div className="heading">
                 <Bounce left>
                     <h1>Works</h1>
                 </Bounce>
             </div>
-            <div className="right-img">
-                <a href="https://www.azaelindia.com" target="_blank" >
-                    <img src={azaelindia} alt="Azael HomePage" />
-                </a>
-            </div>
-        </div>
 
-        <div className="guest">
-            <div className="content"> 
-            <a href="https://www.dwarf.ga" target="_blank">
-                <img src={dwarf}  alt="Dwarf Company"/>
-            </a>
+            <section className="container">
+        
+            </section>
+            <section className="container"></section>
+            <section className="container"></section>
 
-            </div>
-        </div>
-
-        <div className="paying">
-            <div className="content"> 
-            <a href="http://www.payingguest.ml" target="_blank">
-                <img src={payingguest}  alt="Paying Gueset"/>
-            </a>
-
-            </div>
-        </div>
+     
         </div>
     )
 }
