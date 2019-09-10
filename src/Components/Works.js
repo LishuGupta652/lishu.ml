4691406f95a9ce99569192002e802ecf2e806daa
lishu gupta <lishugupta652@gmail.com>
2019-09-10 18:38:30 +0530

    works completed

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -9,17 +9,53 @@ const Works = () => {
     return (   
     <div className="work">
         
-            <div className="heading">
+            <div className="heading container">
                 <Bounce left>
                     <h1>Works</h1>
                 </Bounce>
             </div>
 
-            <section className="container">
-        
+            <section className="wrapper wrapper-1">
+                <div className="container">
+                    <div className="details">
+                        <h3>Azael India</h3>
+                        <p>Organization Operating In The Field of manufacturing, fabrication and trading of mobile health service vans, special concept vehicles, security surveillance vans, health centers and medical equipment.</p>
+                    </div>
+                    <div className="img">
+                        <a href="https://www.azaelindia.com" target="_blank" >
+                            <img src={azaelindia} alt="Azael India" />
+                        </a>
+                    </div>
+                </div>
             </section>
-            <section className="container"></section>
-            <section className="container"></section>
+
+            <section className="wrapper wrapper-2">
+                <div className="container">
+                    <div className="img">
+                        <a href="https://www.dwarf.ga" target="_blank" >
+                            <img src={dwarf} alt="The Dwarf Company" />
+                        </a>
+                    </div>
+                    <div className="details">
+                        <h3>The Dwarf Company</h3>
+                        <p>Startup's are love.</p>
+                    </div>
+                </div>
+            </section>
+            
+            <section className="wrapper wrapper-3">
+                <div className="container">
+                    <div className="details">
+                        <h3>Paying Guest</h3>
+                        <p>Mentionable Project</p>
+                    </div>
+                    <div className="img">
+                        <a href="http://www.payingguest.ml" target="_blank" >
+                            <img src={payingguest} alt="Paying Guest" />
+                        </a>
+                    </div>
+                </div>
+            </section>
 
      
         </div>
