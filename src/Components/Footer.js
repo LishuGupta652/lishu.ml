36f779b2e86dfb82aab5fadeb59510385bcc0a56
lishu gupta <lishugupta652@gmail.com>
2020-01-27 09:59:01 +0530

    contact us updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <div>
+  return (
+    <div className="footer">
+      <div>
+        <a href="https://www.github.com/lishugupta652">
+          <i class="fa fa-github" aria-hidden="true"></i>
+        </a>
+        <h1>
+          {" "}
+          <i class="fa fa-quote-left" aria-hidden="true"></i> Don't Judge Me I
+          Am Still Learning.
+        </h1>
+      </div>
 
-            <a href="https://www.github.com/lishugupta652">
-            <i class="fa fa-github" aria-hidden="true"></i>
-            </a>
-            <h1> <i class="fa fa-quote-left" aria-hidden="true"></i> Don't Judge Me I Am Still Learning.</h1>
-            </div>
+      <div className="resume">
+        <a href="https://docs.google.com/document/d/e/2PACX-1vQAYtwHunE6q5hijPSPIjnDbJW6LViny7gwaY08_mH0FNFQgbhYOtP74du28FIRTwMaLXvJ6w8Ghqlb/pub">
+          Resume
+        </a>
+      </div>
+    </div>
+  );
+};
 
-            <div className="resume">
-                <a href="https://docs.google.com/document/d/e/2PACX-1vQAYtwHunE6q5hijPSPIjnDbJW6LViny7gwaY08_mH0FNFQgbhYOtP74du28FIRTwMaLXvJ6w8Ghqlb/pub">
-                    Resume
-                </a>
-            </div>
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,64 +1,69 @@
-import React from 'react'
+import React from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
-import Image_icon from "../image/hero-img.jpg"
+import Image_icon from "../image/hero-img.jpg";
 
 // react-reveal
-import HeadShake from 'react-reveal/HeadShake';
+import HeadShake from "react-reveal/HeadShake";
+
+import Particles from "react-particles-js";
 
 const Header = () => {
-    return (
-        <>
-        <header>
-            <div className="app">
+  return (
+    <>
+      <header>
+        <div>
+          <div class="container">Your content here...</div>
 
+          <canvas class="background"></canvas>
+        </div>
+        {/* <div className="banner">
+          <div style={{ height: "90vh", overflow: "", position: "relative" }}>
+            <svg
+              viewBox="0 0 500 150"
+              preserveAspectRatio="none"
+              style={{ height: "100%", width: "100%" }}
+            >
+              <path
+                d="M0.00,130.27 C216.00,182.92 300.30,60.39 500.00,139.03 L500.00,0.00 L0.00,0.00 Z"
+                style={{ stroke: "none", fill: "#333652" }}
+              ></path>
+            </svg>
+
+            <div className="hero">
+              <div className="hero-content">
+                <img src={Image_icon} alt="lishu Gupta" />
+                <HeadShake>
+                  <h2 className="name">Lishu Gupta</h2>
+                </HeadShake>
+
+                <i class="devicon-android-plain colored"></i>
+                <i class="devicon-java-plain colored"></i>
+                <i class="devicon-javascript-plain colored"></i>
+                <i class="devicon-react-original colored react-rotate"></i>
+                <i class="devicon-heroku-original colored"></i>
+                <i class="devicon-html5-plain colored"></i>
+                <i class="devicon-python-plain colored"></i>
+
+                <p className="info">Web Developer and Programmer.</p>
+                <p className="info">*Blogger Anonymously</p>
+                <Link
+                  to="skill"
+                  spy={true}
+                  smooth={true}
+                  hashSpy={true}
+                  offset={-10}
+                  duration={500}
+                  delay={300}
+                >
+                  <i class="fa fa-angle-down down-icon" aria-hidden="true"></i>
+                </Link>
+              </div>
             </div>
-                
-                <div className="banner">
+          </div>
+        </div> */}
+      </header>
+    </>
+  );
+};
 
-                <div style={{height: "90vh", overflow: "", position: "relative"}} >
-                    <svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: "100%", width: "100%"}}>
-                        <path d="M0.00,130.27 C216.00,182.92 300.30,60.39 500.00,139.03 L500.00,0.00 L0.00,0.00 Z" style={{stroke: "none", fill: "#333652"}}></path>
-                    </svg>
-
-                <div className="hero">
-                    <div className="hero-content">
-                        <img src={Image_icon} alt="lishu Gupta"/>
-                        <HeadShake>
-                        <h2 className="name">Lishu Gupta</h2>
-                        </HeadShake>
-
-                        <i class="devicon-android-plain colored"></i>
-                        <i class="devicon-java-plain colored"></i>
-                        <i class="devicon-javascript-plain colored"></i>
-                        <i class="devicon-react-original colored react-rotate"></i>
-                        <i class="devicon-heroku-original colored"></i>
-                        <i class="devicon-html5-plain colored"></i>
-                        <i class="devicon-python-plain colored"></i>
-
-
-                        <p className="info">Web Developer and Programmer.</p>
-                        <p className="info">*Blogger Anonymously</p>
-                        <Link 
-                              to="skill"
-                              spy={true}
-                              smooth={true}
-                              hashSpy={true}
-                              offset={-10}
-                              duration={500}
-                              delay={300} >
-                                    <i class="fa fa-angle-down down-icon" aria-hidden="true"></i>
-                        </Link>                        
-                    </div>
-                </div>
-
-
-                </div>
-                
-                
-            </div>
-        </header>
-        </>
-    )
-}
-
-export default Header
+export default Header;
