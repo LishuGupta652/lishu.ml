6c01f6b4d6cc79bde10b344f39b0c9d56f619b12
lishu gupta <lishugupta652@gmail.com>
2020-07-25 14:55:08 +0530

    update flag

--- README.md
+++ README.md
@@ -8,12 +8,12 @@
 
 - [x] Remove unused library
 - [x] Update favicon
-- [ ] Remove flag
+- [x] Remove flag
+- [x] Update Footer Texts
 - [ ] Update Skills Component
 - [ ] Remove Header
 - [ ] Add new header
 - [ ] Update Header text
 - [ ] User Sass instead of Css (npm instal node-sass)
 - [ ] Change scroll bar width
-- [ ] Update Footer Texts
 - [ ] Update Color Schemes
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Flag from "../image/flag.svg";
-import Zoom from "react-reveal/Zoom";
 
 const Footer = () => {
   return (
@@ -11,25 +9,17 @@ const Footer = () => {
             <i class="fa fa-github" aria-hidden="true"></i>
           </a>
         </div>
-        {/* <h1>
+        <h1>
           <i class="fa fa-quote-left" aria-hidden="true"></i> Don't Judge Me I
           Am Still Learning.
-        </h1> */}
-        <h1 className="orange">VANDE MATRAM</h1>
-        <h1 className="white">JAI HIND</h1>
-        <h1 className="green">JAI BHARAT</h1>
+        </h1>
       </div>
 
-      <div className="resume">
+      {/* <div className="resume">
         <a href="https://docs.google.com/document/d/e/2PACX-1vQAYtwHunE6q5hijPSPIjnDbJW6LViny7gwaY08_mH0FNFQgbhYOtP74du28FIRTwMaLXvJ6w8Ghqlb/pub">
           Resume
         </a>
-      </div>
-      <div class="flag-container">
-        <Zoom right>
-          <img alt="flag" src={Flag} />
-        </Zoom>
-      </div>
+      </div> */}
     </div>
   );
 };
