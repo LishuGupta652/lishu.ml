a5aa2ce78897a7ef0ef8aba97b54d59ce6dbbf91
hitgitman <hitgitman@gmail.com>
2020-09-21 10:08:36 +0000

    Added suspense and lazy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Login from "./pages/login";
-import About from "./pages/about";
-import Test from "./pages/test";
-import NotFound from "./pages/notfound";
-
 // CSS IMPORTS
 import "./css/App.scss";
 import "./css/UI.scss";
 import "./css/about.scss";
 import "./css/test.scss";
+
+// Components
+import Home from "./pages/home";
+import Login from "./pages/login";
+import About from "./pages/about";
+import Test from "./pages/test";
+import NotFound from "./pages/notfound";
 
 function App() {
   return (
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import PageProgress from "react-page-progress";
 
 //Components Imports
 import Header from "../Components/Header";
 // import Skills from "../Components/Skills";
-import Works from "../Components/Works";
-import Footer from "../Components/Footer";
+// import Works from "../Components/Works";
+// import Footer from "../Components/Footer";
+
+// const Header = lazy(() => import("../Components/Header"));
+const Works = lazy(() => import("../Components/Works"));
+const Footer = lazy(() => import("../Components/Footer"));
 
 function Home() {
   return (
-    <div>
-      <PageProgress color={"skyblue"} height={5} />
+    <>
+      <Suspense fallback={<div className="loading-suspense">LOADING</div>}>
+        <PageProgress color={"skyblue"} height={5} />
+      </Suspense>
       <Header />
       {/* <Skills /> */}
-      <Works />
-      <Footer />
-    </div>
+
+      <Suspense fallback={<div />}>
+        <Works />
+      </Suspense>
+      <Suspense fallback={<div />}>
+        <Footer />
+      </Suspense>
+    </>
   );
 }
 
