c3b98ffc4a61deefe4a5f014e69f1ae25b438d72
LishuGupta652 <lishugupta652@gmail.com>
2021-11-20 15:28:53 +0000

    fixed motion

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -45,18 +45,18 @@ const Works = () => {
     );
 
     let proxy = { skew: 0 },
-      skewSetter = gsap.quickSetter(".skewElem", "skewY", "deg"), // fast
-      clamp = gsap.utils.clamp(-2, 2); // don't let the skew go beyond 20 degrees.
+      skewSetter = gsap.quickSetter(".work", "skewY", "deg"), // fast
+      clamp = gsap.utils.clamp(-3, 3); // don't let the skew go beyond 20 degrees.
 
     ScrollTrigger.create({
       onUpdate: (self) => {
-        let skew = clamp(self.getVelocity() / -10);
+        let skew = clamp(self.getVelocity() / -500);
         // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
         if (Math.abs(skew) > Math.abs(proxy.skew)) {
           proxy.skew = skew;
           gsap.to(proxy, {
             skew: 0,
-            duration: .3,
+            duration: .8,
             ease: "power4",
             overwrite: true,
             onUpdate: () => skewSetter(proxy.skew),
@@ -66,7 +66,7 @@ const Works = () => {
     });
 
     // make the right edge "stick" to the scroll bar. force3D: true improves performance
-    gsap.set(".skewElem", { transformOrigin: "right center", force3D: true });
+    gsap.set(".work", { transformOrigin: "right center", force3D: true });
   }, []);
 
   //   const intersection = useIntersection(sectionRef, {
